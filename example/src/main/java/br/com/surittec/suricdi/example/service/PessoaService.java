83cc6fe41c97561eefa6c2dbf131662548ec3b8f
felipe.castanon <felipe.castanon@surittec.com.br>
2014-10-03 18:39:16 -0300

    Documentação do exemplo

--- example/src/main/java/br/com/surittec/suricdi/example/service/PessoaService.java
+++ example/src/main/java/br/com/surittec/suricdi/example/service/PessoaService.java
@@ -1,3 +1,23 @@
+/*
+ * SURITTEC
+ * Copyright 2014, SURITTEC CONSULTORIA LTDA, 
+ * and individual contributors as indicated by the @authors tag
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package br.com.surittec.suricdi.example.service;
 
 import java.util.List;
@@ -5,8 +25,8 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
-import br.com.surittec.suricdi.core.exception.BusinessException;
 import br.com.surittec.suricdi.core.service.EntityService;
+import br.com.surittec.suricdi.core.util.Assert;
 import br.com.surittec.suricdi.example.domain.entity.Pessoa;
 import br.com.surittec.suricdi.example.domain.repository.GenericEntityRepository;
 import br.com.surittec.suricdi.example.domain.repository.PessoaRepository;
@@ -42,9 +62,8 @@ public class PessoaService extends EntityService{
 	}
 	
 	public void save(Pessoa pessoa){
-		if(!pessoaRepository.isUnique(pessoa)){
-			throw new BusinessException(messages.pessoaCadastroFalhaUnicidade());
-		}
+		Assert.isFalse(pessoaRepository.isUnique(pessoa), messages.pessoaCadastroFalhaUnicidade());
+		
 		super.save(pessoa);
 	}
 }
