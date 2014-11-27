864ad91c6e017f9d092248c58e65054331704109
llins <lucas.lins@surittec.com.br>
2014-11-27 15:35:58 -0200

    Melhorias no rewrite e nova versão

---
+++ faces/src/main/java/br/com/surittec/suricdi/faces/rewrite/condition/JAASRoles.java
@@ -0,0 +1,69 @@
+/*
+ * SURITTEC
+ * Copyright 2014, SURITTEC CONSULTORIA LTDA, 
+ * and individual contributors as indicated by the @authors tag
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package br.com.surittec.suricdi.faces.rewrite.condition;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.ocpsoft.rewrite.context.EvaluationContext;
+import org.ocpsoft.rewrite.servlet.config.HttpCondition;
+import org.ocpsoft.rewrite.servlet.http.event.HttpServletRewrite;
+
+/**
+ * Condicao para validacao se o usuario nao possui as regras informadas
+ * 
+ * @author Lucas Lins
+ *
+ */
+public class JAASRoles extends HttpCondition {
+
+	private final Collection<String> roles;
+
+	/**
+	 * Create a new {@link JAASRoles} condition specifying the roles of which
+	 * the current user must be a member for evaluation to return
+	 * <code>true</code>.
+	 */
+	public static JAASRoles hasntRoles(String... roles) {
+		return new JAASRoles(roles);
+	}
+
+	private JAASRoles(String[] roles) {
+		this.roles = Arrays.asList(roles);
+	}
+
+	@Override
+	public boolean evaluateHttp(HttpServletRewrite event, EvaluationContext context) {
+		HttpServletRewrite rewrite = event;
+
+		boolean hasAllRoles = true;
+		
+		// check if user has all required roles
+		for (String role : roles) {
+			if (!rewrite.getRequest().isUserInRole(role)) {
+				hasAllRoles = false;
+			}
+		}
+		
+		return !hasAllRoles;
+	}
+
+}
---
+++ faces/src/main/java/br/com/surittec/suricdi/faces/rewrite/handler/RolesRequiredHandler.java
@@ -0,0 +1,71 @@
+/*
+ * SURITTEC
+ * Copyright 2014, SURITTEC CONSULTORIA LTDA, 
+ * and individual contributors as indicated by the @authors tag
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package br.com.surittec.suricdi.faces.rewrite.handler;
+
+import org.ocpsoft.rewrite.annotation.Join;
+import org.ocpsoft.rewrite.annotation.api.ClassContext;
+import org.ocpsoft.rewrite.annotation.api.HandlerChain;
+import org.ocpsoft.rewrite.annotation.handler.HandlerWeights;
+import org.ocpsoft.rewrite.annotation.spi.AnnotationHandler;
+import org.ocpsoft.rewrite.servlet.config.SendStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import br.com.surittec.suricdi.faces.rewrite.annotation.RolesRequired;
+import br.com.surittec.suricdi.faces.rewrite.condition.JAASRoles;
+
+/**
+ * Handler para insersao de regra de seguranca
+ * 
+ * @author Lucas Lins
+ *
+ */
+public class RolesRequiredHandler implements AnnotationHandler<RolesRequired> {
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
+	@Override
+	public Class<RolesRequired> handles() {
+		return RolesRequired.class;
+	}
+
+	@Override
+	public int priority() {
+		return HandlerWeights.WEIGHT_TYPE_STRUCTURAL;
+	}
+
+	@Override
+	public void process(ClassContext context, RolesRequired annotation, HandlerChain chain) {
+		
+		Join join = context.getJavaClass().getAnnotation(Join.class);
+		
+		if(join != null){
+			context.setBaseRule(org.ocpsoft.rewrite.servlet.config.rule.Join.path(join.path()).to(join.to()).withChaining());
+			context.getRuleBuilder().withPriority(annotation.priority());
+			context.getRuleBuilder().when(JAASRoles.hasntRoles(annotation.value()));
+			context.getRuleBuilder().perform(SendStatus.error(403));
+		}else{
+			logger.warn(String.format("SECURITY VULNERABILITY: The class %s must have org.ocpsoft.rewrite.annotation.Join annotation"));
+		}
+		
+		chain.proceed();
+	}
+}
