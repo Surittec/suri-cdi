1ac7fbc290568d030c67295e60af448482930f8d
Lucas Lins <lucas.lins@surittec.com.br>
2015-08-06 17:04:17 -0300

    Inclusão do suporte a AWS SQS

---
+++ core/src/main/java/br/com/surittec/suricdi/core/util/qualifier/Unqualifier.java
@@ -0,0 +1,21 @@
+package br.com.surittec.suricdi.core.util.qualifier;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.inject.Qualifier;
+
+@Qualifier
+@Documented
+@Retention(RUNTIME)
+@Target({ TYPE, METHOD, PARAMETER, FIELD })
+public @interface Unqualifier {
+
+}
---
+++ sqs/src/main/java/br/com/surittec/suricdi/sqs/pool/ConnectionPool.java
@@ -0,0 +1,42 @@
+/*
+ * SURITTEC
+ * Copyright 2015, TTUS TECNOLOGIA DA INFORMACAO LTDA, 
+ * and individual contributors as indicated by the @authors tag
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package br.com.surittec.suricdi.sqs.pool;
+
+import javax.jms.Connection;
+import javax.jms.JMSException;
+import javax.jms.Session;
+
+import org.apache.activemq.jms.pool.SessionKey;
+
+import com.amazon.sqs.javamessaging.SQSSession;
+
+
+public class ConnectionPool extends org.apache.activemq.jms.pool.ConnectionPool {
+
+	public ConnectionPool(Connection connection) {
+		super(connection);
+	}
+
+	@Override
+	protected Session makeSession(SessionKey key) throws JMSException {
+        return new br.com.surittec.suricdi.sqs.jms.SQSSession((SQSSession)connection.createSession(key.isTransacted(), key.getAckMode()));
+    }
+}
